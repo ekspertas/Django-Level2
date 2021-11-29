49481e9b173c6ea82a04c069380bddb6a70c83be
ekspertas <ekspertasd@gmail.com>
2021-11-29 17:20:39 +0200

    ДЗ к уроку 3

---
+++ ordersapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from ordersapp import views as ordersapp
+
+app_name = 'ordersapp'
+
+urlpatterns = [
+    path('', ordersapp.OrderListView.as_view(), name='list'),
+    path('read/<pk>/', ordersapp.OrderListView.as_view(), name='read'),
+    path('create/', ordersapp.OrderCreateView.as_view(), name='create'),
+    path('update/<pk>/', ordersapp.OrderUpdateView.as_view(), name='update'),
+    path('delete/<pk>/', ordersapp.OrderListView.as_view(), name='delete'),
+]
