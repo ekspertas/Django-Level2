57fccac50967937b085032fe54674e04fe1c11cc
ekspertas <ekspertasd@gmail.com>
2021-12-16 18:39:09 +0200

    debug_toolbar fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 
 if ENV_TYPE == 'local':
     STATICFILES_DIRS = (
-        BASE_DIR / 'static',
+        os.path.join(BASE_DIR, 'static'),
     )
 else:
     STATIC_ROOT = BASE_DIR / 'static'
@@ -205,26 +205,26 @@ SOCIAL_AUTH_PIPELINE = (
 )
 
 if DEBUG:
-   def show_toolbar(request):
-       return True
+    def show_toolbar(request):
+        return True
 
-   DEBUG_TOOLBAR_CONFIG = {
-       'SHOW_TOOLBAR_CALLBACK': show_toolbar,
-   }
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
 
-   DEBUG_TOOLBAR_PANELS = [
-       'debug_toolbar.panels.versions.VersionsPanel',
-       'debug_toolbar.panels.timer.TimerPanel',
-       'debug_toolbar.panels.settings.SettingsPanel',
-       'debug_toolbar.panels.headers.HeadersPanel',
-       'debug_toolbar.panels.request.RequestPanel',
-       'debug_toolbar.panels.sql.SQLPanel',
-       'debug_toolbar.panels.templates.TemplatesPanel',
-       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-       'debug_toolbar.panels.cache.CachePanel',
-       'debug_toolbar.panels.signals.SignalsPanel',
-       'debug_toolbar.panels.logging.LoggingPanel',
-       'debug_toolbar.panels.redirects.RedirectsPanel',
-       'debug_toolbar.panels.profiling.ProfilingPanel',
-       'template_profiler_panel.panels.template.TemplateProfilerPanel',
-   ]
+    DEBUG_TOOLBAR_PANELS = [
+           'debug_toolbar.panels.versions.VersionsPanel',
+           'debug_toolbar.panels.timer.TimerPanel',
+           'debug_toolbar.panels.settings.SettingsPanel',
+           'debug_toolbar.panels.headers.HeadersPanel',
+           'debug_toolbar.panels.request.RequestPanel',
+           'debug_toolbar.panels.sql.SQLPanel',
+           'debug_toolbar.panels.templates.TemplatesPanel',
+           'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+           'debug_toolbar.panels.cache.CachePanel',
+           'debug_toolbar.panels.signals.SignalsPanel',
+           'debug_toolbar.panels.logging.LoggingPanel',
+           'debug_toolbar.panels.redirects.RedirectsPanel',
+           'debug_toolbar.panels.profiling.ProfilingPanel',
+           'template_profiler_panel.panels.template.TemplateProfilerPanel',
+       ]
